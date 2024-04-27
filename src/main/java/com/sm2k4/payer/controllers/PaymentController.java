9ded875a4f0a696b246e6dbd9bdcf4fbfc52dda6
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-04-27 19:15:12 +0530

    creates PaymentController.

--- src/main/java/com/sm2k4/payer/controllers/PaymentController.java
+++ src/main/java/com/sm2k4/payer/controllers/PaymentController.java
@@ -1,6 +1,8 @@
 package com.sm2k4.payer.controllers;
 
+import com.razorpay.RazorpayException;
 import com.sm2k4.payer.dtos.InitiatePaymentRequestDTO;
+import com.sm2k4.payer.services.PaymentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
+    private PaymentService paymentService;
 
-    @PostMapping("/")
-    public String initiatePayment(@RequestBody InitiatePaymentRequestDTO requestDTO){
-        return
+
+    PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
+    @PostMapping("/initiate")
+    public String initiatePayment(@RequestBody InitiatePaymentRequestDTO requestDto) throws RazorpayException {
+        return paymentService.initiatePayment(requestDto.getOrderId(), requestDto.getEmail(),requestDto.getName(),requestDto.getPhoneNumber());
+    }
+
 }
